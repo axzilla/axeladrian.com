9d68d5799c09ab1ae0ed069524e741368c63312d
“axzilla” <hello@axeladrian.com>
2024-08-05 12:15:04 +0200

    fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import icon from "astro-icon";
 import node from "@astrojs/node";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  // site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind(), icon()],
   output: "server",
   adapter: node({
